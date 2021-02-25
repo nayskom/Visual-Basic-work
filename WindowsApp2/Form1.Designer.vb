528bbd5e4d89127b2dc235c4ec879e7468a0d7be
Nayskom <Nayskom@gmail.com>
2021-02-26 00:10:40 +0300

    Добавьте файлы проекта.

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Z = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.X = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Y = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(122, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Мочалов Н.А."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(122, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Группа ИСТ-2020"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(122, 171)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Вариант 7"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(122, 224)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(148, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Начальное значение"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(122, 272)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(140, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Конечное значение"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(321, 224)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(324, 22)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(321, 272)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(324, 22)
+        Me.TextBox2.TabIndex = 6
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Z, Me.X, Me.Y})
+        Me.DataGridView1.Location = New System.Drawing.Point(321, 38)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(324, 150)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(513, 373)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Табулировать"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(321, 318)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(324, 22)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(122, 318)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(32, 17)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Шаг"
+        '
+        'Z
+        '
+        Me.Z.HeaderText = "Z"
+        Me.Z.MinimumWidth = 6
+        Me.Z.Name = "Z"
+        Me.Z.Width = 125
+        '
+        'X
+        '
+        Me.X.HeaderText = "X"
+        Me.X.MinimumWidth = 6
+        Me.X.Name = "X"
+        Me.X.Width = 125
+        '
+        'Y
+        '
+        Me.Y.HeaderText = "Y"
+        Me.Y.MinimumWidth = 6
+        Me.Y.Name = "Y"
+        Me.Y.Width = 125
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(682, 450)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Задание 3"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Z As DataGridViewTextBoxColumn
+    Friend WithEvents X As DataGridViewTextBoxColumn
+    Friend WithEvents Y As DataGridViewTextBoxColumn
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Z As Single, X As Single, Y(6) As Single
+        Dim A As Single, B As Single, h As Single
+        Dim i As Integer
+        A = Val(TextBox1.Text)
+        B = Val(TextBox2.Text)
+        h = Val(TextBox3.Text)
+        i = 0
+        For Z = A To B Step h
+            X = 3 * Z ^ 3
+            i = i + 1
+            If X < 1 Then
+                Y(i) = X ^ 2 * Math.Exp(-X)
+            Else
+                Y(i) = Math.Log(X ^ 2) / (X * (1 + X))
+            End If
+            Dim index = DataGridView1.Rows.Add()
+            DataGridView1.Rows.Item(index).Cells(0).Value = Z.ToString("0.0")
+            DataGridView1.Rows.Item(index).Cells(1).Value = X.ToString("0.000")
+            DataGridView1.Rows.Item(index).Cells(2).Value = Y(i).ToString("0.000")
+        Next Z
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
